29928ff622404826d00071b40c5c4750b9f85591
Hasan Çobanoğlu <hasancobanoglu93@gmail.com>
2022-04-12 14:12:11 +0300

    HC ilk commit team03

---
+++ src/deneme/CO1.java
@@ -0,0 +1,7 @@
+package deneme;
+
+public class CO1 {
+    public static void main(String[] args) {
+        System.out.println("Merhaba Team03 Branch'imi olusturdum");
+    }
+}
---
+++ src/deneme/sameFirstAndLast.java
@@ -0,0 +1,65 @@
+package deneme;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class sameFirstAndLast {
+   /*
+
+    Given an array of ints,
+    print true if the array is length 1 or more, and the first element and the last element are equal.
+
+    sameFirstLast([1, 2, 3]) → false
+    sameFirstLast([1, 2, 3, 1]) → true
+    sameFirstLast([1, 2, 1]) → true
+    sameFirstLast([1]) → true
+
+    print just true or false
+
+    */
+
+
+    /*
+
+    Bir int array verildiğinde, array uzunluğu 1 veya daha fazlaysa ve ilk öğe ile
+    son öğe eşitse true değerini yazdırın.
+
+    sameFirstLast([1, 2, 3]) → false
+    sameFirstLast([1, 2, 3, 1]) → true
+    sameFirstLast([1, 2, 1]) → true
+    sameFirstLast([1]) → true
+
+    sadece  true veya false print et
+
+    */
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Array elemabları gir");
+
+        String myStr = scan.nextLine();
+
+        String[] useThisArray = myStr.split(" ");
+
+        System.out.println(Arrays.toString(useThisArray));
+
+        System.out.println("useThisArray nin uzunluğu: "+useThisArray.length);
+        // code start here
+
+        // Koda burdan başlayin
+
+        if(useThisArray.length>=1 && useThisArray[0].equals(useThisArray[useThisArray.length-1])){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+
+
+
+    }
+
+    }
+
+
+
