da951a7fa837246b77a302aa6aca8e14e88674d2
Busra Boyali <bsrbyl.bus@gmail.com>
2022-04-13 23:57:47 +0300

    13.04.22

--- src/deneme/deneme.java
+++ src/deneme/deneme.java
@@ -4,5 +4,6 @@ public class deneme {
     public static void main(String[] args) {
         System.out.println("deneme ");
         System.out.println("busra b");
+        System.out.println("...");
     }
 }
