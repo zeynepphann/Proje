c5cc17451998d78dd54fd17c2998848d39cb78b2
Zeynep HAN <zeynephan9906@gmail.com>
2022-04-14 00:48:25 +0300

    14.04.2022

--- src/deneme/deneme.java
+++ src/deneme/deneme.java
@@ -3,10 +3,7 @@ package deneme;
 public class deneme {
     public static void main(String[] args) {System.out.println("deneme ");
         System.out.println("eda deneme");
-<<<<<<< HEAD
         System.out.println("busra");
-=======
         System.out.println("zeynep");
->>>>>>> a5e32010b5c70166859d79b5c6bf684719a37a72
     }
 }
