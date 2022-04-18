a0d46b282995e8607ecd41fe3c8c91801b7480a2
Zeynep HAN <zeynephan9906@gmail.com>
2022-04-18 21:54:56 +0300

    18.04.2022

--- src/kitapYonetimi/TumKitaplariListele.java
+++ src/kitapYonetimi/TumKitaplariListele.java
@@ -1,4 +1,22 @@
 package kitapYonetimi;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class TumKitaplariListele {
+    public static void kitapListesi(){
+
+
+        System.out.println("************** KITAP LISTENIZ ************** ");
+        for (EklenenKitaplar  each : KitapEkle.kitaplar
+        ) {
+            System.out.println("Kitap No: " +  each.kitapNo +
+                    ", Kitap Adı: " + each.kitapAdi +
+                    ", Yazar Adı: "+ each.yazarAdi+
+                    ", Fiyatı: " + each.kitapFiyati);
+        }
+    }
 }
+
+
+
