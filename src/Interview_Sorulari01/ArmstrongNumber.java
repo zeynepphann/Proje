6b36e8554ffda971591f9eba65da962ab4a10167
Zeynep HAN <zeynephan9906@gmail.com>
2022-05-06 12:21:59 +0300

    06.05.2022

--- src/Interview_Sorulari01/ArmstrongNumber.java
+++ src/Interview_Sorulari01/ArmstrongNumber.java
@@ -1,46 +1,44 @@
 package Interview_Sorulari01;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public class ArmstrongNumber {
+
+
+    /*
+     * Java 8 / Lambda approach to generate Armstrong number.
+     * Armstrong always start to look from number 1.
+     * @param series Number of how many Armstrong number should be generated
+     * @return List holding resulting Armstrong number.
+     */
+
+    public static List<Integer> sayilar(int sayi) {
+
+        return Stream.iterate(1, i -> ++i)
+                .filter(i -> i == Stream.of(String.valueOf(i).split(""))
+                        .map(Integer::valueOf)
+                        .map(n -> (n*n*n))
+                        .mapToInt(n -> n)
+                        .sum())
+                .limit(sayi)
+                .collect(toList());
+    }
+
     public static void main(String[] args) {
-
-          /*  soru1: Check if the integer is an Armstrong numbers to 4 digit numbers(4 digit-> exclusive)
-         Armstrong sayi:herhangi bir 3 basamakli sayinin rakamlarinin kupleri toplami o sayiyi veriyorsa sayi  Armstrong sayidir
-            (0, 1, 153, 370, 371, 407)
-            153 = 1*1*1 + 5*5*5 + 3*3*3 = 153
-            370 = 3*3*3 + 7*7*7 + 0*0*0 = 370
-
-          soru2: Birden baslayarak girilen sayıya kadar her bir tamsayının amstrong sayı olup olmadıgını
-          gösteren program yazınız */
-        List<Integer> sayi = new ArrayList<>();
-        Scanner scan= new Scanner(System.in);
-        System.out.println("Lutfen bir sayi giriniz");
-
-
-        armstrongSayi(sayi);
-        kontrolIslemi(sayi);
-
-
+        List<Integer> sayilar = ArmstrongNumber.sayilar(5);
+        System.out.println(sayilar);
     }
 
-    private static void kontrolIslemi(List<Integer> sayi) {
-        for (int i = 0; i <sayi.size() ; i++) {
 
-        }
 
-    }
-
-    // allMatch(t -> t.length() <= 7) ? "list elemanları 7 ve daha az harften olusuyor" : "list elemanları 7 harften  buyuk");
-
-    private static void armstrongSayi(List<Integer> sayi) {
-        //sayi.
-            //    stream().map(t->t*t*t).allMatch()
-
-    }
 
 
 }
+
+
+
+
+
